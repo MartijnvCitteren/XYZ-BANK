5c1065830dafadd2d725bf73eaff399c1c418d89
martijnvancitteren <martijnvancitteren@live.nl>
2025-07-05 12:11:40 +0200

    added log4j2

--- onboarding-ws/src/main/java/com/xyz_bank/onboarding/OnboardingApplication.java
+++ onboarding-ws/src/main/java/com/xyz_bank/onboarding/OnboardingApplication.java
@@ -1,13 +1,16 @@
 package com.xyz_bank.onboarding;
 
+import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Log4j2
 public class OnboardingApplication {
-
 	public static void main(String[] args) {
+		log.info("Starting Onboarding Application");
 		SpringApplication.run(OnboardingApplication.class, args);
+		log.info("Onboarding Application started successfully");
 	}
 
 }
