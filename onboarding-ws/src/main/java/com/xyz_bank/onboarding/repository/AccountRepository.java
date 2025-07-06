2067a712abeb0d0e5bf2cc2a317072f72e298a9b
martijnvancitteren <martijnvancitteren@live.nl>
2025-07-06 09:03:32 +0200

    Added repositories

---
+++ onboarding-ws/src/main/java/com/xyz_bank/onboarding/repository/AccountRepository.java
@@ -0,0 +1,11 @@
+package com.xyz_bank.onboarding.repository;
+
+import com.xyz_bank.onboarding.model.Account;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface AccountRepository extends JpaRepository<Account, UUID> {
+}
