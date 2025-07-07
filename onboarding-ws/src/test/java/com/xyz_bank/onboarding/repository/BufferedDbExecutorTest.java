d365300da0b90165a7e05486dd2c82030fca5397
martijnvancitteren <martijnvancitteren@live.nl>
2025-07-07 21:36:04 +0200

    4- refactor: Adding Test for BufferdDBExcutor - Looks like SumbitWithResult Doesnt works as expected.

---
+++ onboarding-ws/src/test/java/com/xyz_bank/onboarding/repository/BufferedDbExecutorTest.java
@@ -0,0 +1,58 @@
+package com.xyz_bank.onboarding.repository;
+
+import com.xyz_bank.onboarding.exception.BufferedDbException;
+import com.xyz_bank.onboarding.factory.CustomerFactory;
+import com.xyz_bank.onboarding.model.Customer;
+import com.xyz_bank.onboarding.repository.customer.CustomerRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+import java.util.UUID;
+import java.util.function.Supplier;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+class BufferedDbExecutorTest {
+    @Mock
+    private CustomerRepository customerRepository;
+
+    @InjectMocks
+    private BufferedDbExecutor bufferedDbExecutor;
+
+    private static final int MAX_QUEUE_SIZE = 60;
+
+    @Test
+    void givenQueueReachedMaxDept_whenSubmitRunnable_thenThrowBufferdDbException() {
+        assertThrows(BufferedDbException.class, ()-> addNumberOfRunnablesToQueue(MAX_QUEUE_SIZE+15));
+    }
+
+    @Test
+    void givenQueueIsEmpty_whenSubmitWithResult_thenReturnObject() {
+        //given
+        UUID uuid = UUID.randomUUID();
+        Optional<Customer> optionalCustomer = Optional.of(CustomerFactory.createCustomer().id(uuid).build());
+        when(customerRepository.findById(any(UUID.class))).thenReturn(optionalCustomer);
+
+        //when
+        Object result = bufferedDbExecutor.submitWithResult(()-> customerRepository.findById(uuid));
+
+        //then
+        assertEquals(optionalCustomer, result);
+    }
+
+    private void addNumberOfRunnablesToQueue(int numberOfRunnables) {
+        for (int i = 0; i < numberOfRunnables; i++) {
+            bufferedDbExecutor.submit(mock(Runnable.class));
+        }
+    }
+
+
+}
