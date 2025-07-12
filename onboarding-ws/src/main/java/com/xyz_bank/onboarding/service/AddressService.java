4e1f33c5511d193c00d27c99c74654f536dec815
martijnvancitteren <martijnvancitteren@live.nl>
2025-07-12 10:14:49 +0200

    6 -Added AddressService

--- onboarding-ws/src/main/java/com/xyz_bank/onboarding/service/AddressService.java
+++ onboarding-ws/src/main/java/com/xyz_bank/onboarding/service/AddressService.java
@@ -7,7 +7,12 @@ import org.springframework.stereotype.Service;
 @Service
 public class AddressService {
     public Address createAddress(AddressDto addressDto){
-        //todo
-        return null;
+        Address address = new Address();
+        address.setCountry(addressDto.country());
+        address.setCity(addressDto.city());
+        address.setStreet(addressDto.street());
+        address.setZipCode(addressDto.zipcode());
+        address.setHouseNumber(addressDto.houseNumber());
+        return address;
     }
 }
