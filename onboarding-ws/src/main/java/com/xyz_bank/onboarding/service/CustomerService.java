94db6cb42dbde07fb175f89721841f002666c2ec
martijnvancitteren <martijnvancitteren@live.nl>
2025-07-12 15:27:07 +0200

    19 - added caffeine cache to store tokens

--- onboarding-ws/src/main/java/com/xyz_bank/onboarding/service/CustomerService.java
+++ onboarding-ws/src/main/java/com/xyz_bank/onboarding/service/CustomerService.java
@@ -3,32 +3,40 @@ package com.xyz_bank.onboarding.service;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
 import com.xyz_bank.onboarding.exception.LoginException;
 import com.xyz_bank.onboarding.repository.customer.CustomerRepositoryBuffered;
 import com.xyz_bank.onboarding.rest.dto.LoginRequestDto;
 import com.xyz_bank.onboarding.rest.dto.LoginResponseDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
 import org.springframework.stereotype.Service;
 
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 @Service
 @RequiredArgsConstructor
 @Log4j2
 public class CustomerService {
     private final CustomerRepositoryBuffered customerRepositoryBuffered;
+    private final Cache<String, String> inlogCache = Caffeine.newBuilder()
+            .expireAfterWrite(2, TimeUnit.HOURS)
+            .maximumSize(500)
+            .build();
     private Map<String, String> existingUsernamesAndPasswords = Collections.synchronizedMap(new HashMap<>());
 
     public LoginResponseDto login(LoginRequestDto loginRequestDto) {
-        String password = findPassword(loginRequestDto);
-        if(!loginRequestDto.password().equals(password)) {
+        String password = getRegisteredPassword(loginRequestDto);
+        if (!loginRequestDto.password().equals(password)) {
             throw new LoginException("This account does not exist or you use invalid login credentials");
         }
-        return new LoginResponseDto(createToken());
+        String token = createToken();
+        inlogCache.put(token, loginRequestDto.username());
+        return new LoginResponseDto(token);
     }
 
     private String createToken() {
@@ -41,11 +49,12 @@ public class CustomerService {
         }
     }
 
-    private String findPassword(LoginRequestDto loginRequestDto) {
-        if(existingUsernamesAndPasswords.isEmpty()){
+    private String getRegisteredPassword(LoginRequestDto loginRequestDto) {
+        if (existingUsernamesAndPasswords.isEmpty()) {
             existingUsernamesAndPasswords.putAll(customerRepositoryBuffered.getAllUsernamesAndPasswords());
         }
         return existingUsernamesAndPasswords.get(loginRequestDto.username());
     }
+
 
 }
